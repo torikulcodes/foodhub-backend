d9c49497e1fa0af6a149de4d8724a2877c019520
md torikul <mdtorikulislamprogrammer@gmail.com>
2026-04-16 10:44:44 +0600

    delete server.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
 import app from "./app.js";
-export default app;
+import { prisma } from "./lib/prisma.js";
+
+const PORT = Number(process.env.PORT) || 5000;
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("Server running on", PORT);
+    });
+  } catch (error) {
+    console.error("An error occurred:", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
